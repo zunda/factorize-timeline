005cd21074e2f9a615f7f18abaeb15fe3e240b22
zunda <zundan@gmail.com>
2016-10-30 20:08:54 -1000

    Add String#integers

--- exe/factorize-timeline.rb
+++ exe/factorize-timeline.rb
@@ -7,43 +7,51 @@ require 'tw'
 require 'prime'
 
 class Array
-  def reject_dup
-    r = Array.new
-    f = Hash.new
-    self.each do |e|
-      unless f[e]
-        r << e
-        f[e] = true
-      end
-    end
-    return r
-  end
+	def reject_dup
+		r = Array.new
+		f = Hash.new
+		self.each do |e|
+			unless f[e]
+				r << e
+				f[e] = true
+			end
+		end
+		return r
+	end
 end
 
-Tw::Auth.get_or_regist_user(nil)
-self_user = Tw::Conf['default_user']
-
-client = Tw::Client.new
-client.auth
-
-puts "Following user stream and tweeting as @#{self_user}"
-Tw::Client::Stream.new(self_user).user_stream do |tweet|
-	next if tweet.user == self_user
-	next if tweet.text =~ /\ART @/
-	nums = tweet.text.scan(/[\d\.]+/).reject{|e| e =~ /\./}.map{|e| e.to_i}.reject{|e| e <= 1}.reject_dup
-	next if nums.empty?
-
-	factors = nums.map{|n|
-		"#{n}=" + Prime.prime_division(n).reverse.map{|b,e|
-			e > 1 ? "#{b}^#{e}" : b.to_s
-		}.join('×')
-	}
-
-	puts tweet.url
-	puts tweet.text
-	text = factors.shift
-	while not factors.empty? and text.length < 140 - factors[0].length - 1
-		text += " " + factors.shift
+class String
+	def integers
+		self.scan(/[\d\.]+/).reject{|e| e =~ /\./}.map{|e| e.to_i}
 	end
-	client.tweet(text) if text.length < 140
+end
+
+if __FILE__ == $0
+	Tw::Auth.get_or_regist_user(nil)
+	self_user = Tw::Conf['default_user']
+
+	client = Tw::Client.new
+	client.auth
+
+	puts "Following user stream and tweeting as @#{self_user}"
+	Tw::Client::Stream.new(self_user).user_stream do |tweet|
+		next if tweet.user == self_user
+		next if tweet.text =~ /\ART @/
+		nums = tweet.text.integers.reject{|e| e <= 1}.reject_dup
+		next if nums.empty?
+
+		factors = nums.map{|n|
+			"#{n}=" + Prime.prime_division(n).reverse.map{|b,e|
+				e > 1 ? "#{b}^#{e}" : b.to_s
+			}.join('×')
+		}
+
+		puts tweet.url
+		puts tweet.text
+		text = factors.shift
+		while not factors.empty? and text.length < 140 - factors[0].length - 1
+			text += " " + factors.shift
+		end
+		client.tweet(text) if text.length < 140
+	end
 end
