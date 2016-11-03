1856316f2e97cdee97cbf5e1f56c0241cc602171
zunda <zundan@gmail.com>
2016-11-03 08:01:05 -1000

    Allow Ruby style method call to an integer

--- exe/factorize-timeline.rb
+++ exe/factorize-timeline.rb
@@ -22,7 +22,7 @@ end
 
 class String
 	def integers
-		self.scan(/[\d\.]+/).reject{|e| e =~ /\./}.map{|e| e.to_i}
+		self.scan(/[\d\.]+/).reject{|e| e =~ /\.\d/}.map{|e| e.to_i}
 	end
 
 	def rejecrt_urls
